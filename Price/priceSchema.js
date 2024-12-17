c24b1aa632d8eefa16cde99023081d0ed7b08a70
unknown <arafat.innotimes.net@gmail.com>
2024-12-17 14:22:16 +0600

    Price List Route ok

--- Price/priceSchema.js
+++ Price/priceSchema.js
@@ -2,9 +2,20 @@ const mongoose = require("mongoose");
 
 const priceSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    price: { type: String, required: true },
-    status: { type: Boolean, required: true },
+    name: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["Active", "Inactive"],
+      default: 'Active'
+    },
   },
   { timestamps: true }
 );
